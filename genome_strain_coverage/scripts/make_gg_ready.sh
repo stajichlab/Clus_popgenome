78274db20263a88b0d74f61bec341813a7754382
Jason Stajich <jason.stajich@ucr.edu>
2017-05-07 17:58:38 +0200

    modify extension used

--- genome_strain_coverage/scripts/make_gg_ready.sh
+++ genome_strain_coverage/scripts/make_gg_ready.sh
@@ -1,5 +1,5 @@
 for file in plot/*.gene_cov_norm.tab
 do
-	b=$(basename $file)
-	perl scripts/prep_for_ggplot.pl $file > plot/$b.gg.csv
+	b=$(basename $file .tab)
+	perl scripts/prep_for_ggplot.pl $file > plot/$b.gg.tab
 done
