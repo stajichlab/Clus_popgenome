75c0331d68058a21c19088d76a95ed1fb96b31d4
Jason Stajich <jasonstajich.phd@gmail.com>
2017-05-07 08:50:01 -0700

    run from top level dir

--- genome_strain_coverage/scripts/make_gg_ready.sh
+++ genome_strain_coverage/scripts/make_gg_ready.sh
@@ -1,5 +1,5 @@
-for file in *.gene_cov_norm.tab
+for file in plot/*.gene_cov_norm.tab
 do
 	b=$(basename $file)
-	perl prep_for_ggplot.pl $file > $b.gg.csv
+	perl scripts/prep_for_ggplot.pl $file > plot/$b.gg.csv
 done
