f48614bb384d0304b6a84344c1f5e20f9b5e2cc0
Jason Stajich <jasonstajich.phd@gmail.com>
2018-01-05 01:57:46 -0800

    tracks and plots

---
+++ genome_plots/Rscripts/plot_tracks.R
@@ -0,0 +1,20 @@
+library(ggplot2)
+library(RColorBrewer)
+library(colorRamps)
+library(tidyr)
+library(reshape2)
+
+infile = 'tracks/alltracks.bin10000.tab'
+df <- read.table(infile,header=T,sep="\t")
+
+head(df)
+pdffile= 'plots/Clus_density.pdf'
+pdf(pdffile,width=18,height=6)
+Title = "Feature density"
+
+ggplot(df, aes(Window, Density)) + geom_point(aes(color=Chr),
+                                              alpha=1/2,size=0.5) +
+    facet_wrap(~Track, ncol=1,scales="free") + 
+    labs(title=Title,xlab="Chrom position (10kb window)") +
+    scale_colour_brewer(palette = "Dark2")
+
