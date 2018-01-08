a8d7543d63f0b00d816709d564bc2c0c698c70a3
Jason Stajich <jasonstajich.phd@gmail.com>
2018-01-08 11:33:55 -0800

    updated for more cmdline driven config

---
+++ genome_plots/make_plots.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+#SBATCH --nodes 1 --ntasks 1 --out makeplots.log --mem 2G
+
+module load bedtools
+module switch python/3
+python3 ./scripts/bed_to_bins.py --window 20000
+python3 ./scripts/bed_to_bins.py --window 10000
+python3 ./scripts/bed_to_bins.py --window 50000
+Rscript Rscripts/plot_tracks.R
