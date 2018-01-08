7b08bd653be2d73403d0e3125d60c9297dc89cc7
Jason Stajich <jasonstajich.phd@gmail.com>
2018-01-08 11:57:53 -0800

    adding all vs best dataplot

--- genome_plots/Rscripts/plot_tracks.R
+++ genome_plots/Rscripts/plot_tracks.R
@@ -5,14 +5,42 @@ library(colorRamps)
 infile = 'tracks/alltracks.bin10000.tab'
 df <- read.table(infile,header=T,sep="\t")
 
+pdffile= 'plots/Clus_density_10kb_all.pdf'
+pdf(pdffile,width=12,height=6)
+Title = "Feature density"
+df$Chromosome <- df$Chr
+df$Track.order = factor(df$Track,levels = c("Genes","PopA.SNP.lungonly",
+                                            "PopA.SNP.lungonly_curated",
+                                            "PopA.INDEL.lungonly",
+                                            "PopA.INDEL.lungonly_curated",
+                                            "repeats"), labels=c("Genes",
+                                                                 "SNPs","SNPs_curated",
+                                                                 "INDELs",
+                                                                 "INDELs_curated",
+                                                                 "Repeats"))
+
+ggplot(df, aes(Window, Density)) + geom_line(aes(color=Chromosome),
+                                              alpha=1/2,size=0.5) +
+    facet_wrap(~Track.order, ncol=1,scales="free_y")+
+    labs(title=Title,xlab="Chrom position (10kb window)") +
+    scale_colour_brewer(palette = "Dark2") + theme_minimal()
+
+ggplot(df, aes(Window, Density)) + geom_point(aes(color=Chromosome),
+                                              alpha=1/2,size=0.5) +
+    facet_wrap( ~Track.order, ncol=1,scales="free_y") +
+    labs(title=Title,xlab="Chrom position (10kb window)") +
+    scale_colour_brewer(palette = "Dark2") + theme_minimal()
+
 pdffile= 'plots/Clus_density_10kb.pdf'
 pdf(pdffile,width=12,height=5)
 Title = "Feature density"
 df$Chromosome <- df$Chr
-df$Track.order = factor(df$Track,levels = c("Genes","PopA.SNP.lungonly","PopA.INDEL.lungonly",
+df$Track.order = factor(df$Track,levels = c("Genes",
+                                            "PopA.SNP.lungonly_curated",
                                             "PopA.INDEL.lungonly_curated",
-                                            "repeats"), labels=c("Genes","SNPs","INDELs",
-                                                                 "INDELs_curated",
+                                            "repeats"), labels=c("Genes",
+                                                                 "SNPs",
+                                                                 "INDELs",
                                                                  "Repeats"))
 
 ggplot(df, aes(Window, Density)) + geom_line(aes(color=Chromosome),
@@ -32,14 +60,44 @@ infile = 'tracks/alltracks.bin50000.tab'
 df <- read.table(infile,header=T,sep="\t")
 df$Chromosome <- df$Chr
 
+pdffile= 'plots/Clus_density_50kb_all.pdf'
+pdf(pdffile,width=12,height=6)
+Title = "Feature density"
+
+df$Track.order = factor(df$Track,levels = c("Genes","PopA.SNP.lungonly",
+                                            "PopA.SNP.lungonly_curated",
+                                            "PopA.INDEL.lungonly",
+                                            "PopA.INDEL.lungonly_curated",
+                                            "repeats"), labels=c("Genes",
+                                                                 "SNPs","SNPs_curated",
+                                                                 "INDELs",
+                                                                 "INDELs_curated",
+                                                                 "Repeats"))
+
+ggplot(df, aes(Window, Density)) + geom_line(aes(color=Chromosome),
+                                              alpha=1/2,size=0.5) +
+    facet_wrap(~Track.order, ncol=1,scales="free_y") +
+    labs(title=Title,xlab="Chrom position (50kb window)") +
+    scale_colour_brewer(palette = "Dark2") + theme_minimal()
+
+ggplot(df, aes(Window, Density)) + geom_point(aes(color=Chromosome),
+                                              alpha=1/2,size=0.5) +
+    facet_wrap(~Track.order, ncol=1,scales="free_y") +
+    labs(title=Title,xlab="Chrom position (50kb window)") +
+    scale_colour_brewer(palette = "Dark2") + theme_minimal()
+
 pdffile= 'plots/Clus_density_50kb.pdf'
 pdf(pdffile,width=12,height=5)
 Title = "Feature density"
-df$Track.order = factor(df$Track,levels = c("Genes","PopA.SNP.lungonly","PopA.INDEL.lungonly",
+
+df$Track.order = factor(df$Track,levels = c("Genes",
+                                            "PopA.SNP.lungonly_curated",
                                             "PopA.INDEL.lungonly_curated",
-                                            "repeats"), labels=c("Genes","SNPs","INDELs",
-                                                                 "INDELs_curated",
+                                            "repeats"), labels=c("Genes",
+                                                                 "SNPs",
+                                                                 "INDELs",
                                                                  "Repeats"))
+
 ggplot(df, aes(Window, Density)) + geom_line(aes(color=Chromosome),
                                               alpha=1/2,size=0.5) +
     facet_wrap(~Track.order, ncol=1,scales="free_y") +
@@ -57,14 +115,43 @@ infile = 'tracks/alltracks.bin20000.tab'
 df <- read.table(infile,header=T,sep="\t")
 df$Chromosome <- df$Chr
 
+pdffile= 'plots/Clus_density_20kb_all.pdf'
+pdf(pdffile,width=12,height=5)
+Title = "Feature density"
+
+df$Track.order = factor(df$Track,levels = c("Genes","PopA.SNP.lungonly",
+                                            "PopA.SNP.lungonly_curated",
+                                            "PopA.INDEL.lungonly",
+                                            "PopA.INDEL.lungonly_curated",
+                                            "repeats"), labels=c("Genes",
+                                                                 "SNPs","SNPs_curated",
+                                                                 "INDELs",
+                                                                 "INDELs_curated",
+                                                                 "Repeats"))
+
+ggplot(df, aes(Window, Density)) + geom_line(aes(color=Chromosome),
+                                              alpha=1/2,size=0.5) +
+    facet_wrap(~Track.order, ncol=1,scales="free_y") +
+    labs(title=Title,xlab="Chrom position (20kb window)") +
+    scale_colour_brewer(palette = "Dark2") + theme_minimal()
+
+ggplot(df, aes(Window, Density)) + geom_point(aes(color=Chromosome),
+                                              alpha=1/2,size=0.5) +
+    facet_wrap(~Track.order, ncol=1,scales="free_y") +
+    labs(title=Title,xlab="Chrom position (20kb window)") +
+    scale_colour_brewer(palette = "Dark2") + theme_minimal()
+
+
 pdffile= 'plots/Clus_density_20kb.pdf'
 pdf(pdffile,width=12,height=5)
 Title = "Feature density"
 
-df$Track.order = factor(df$Track,levels = c("Genes","PopA.SNP.lungonly","PopA.INDEL.lungonly",
+df$Track.order = factor(df$Track,levels = c("Genes",
+                                            "PopA.SNP.lungonly_curated",
                                             "PopA.INDEL.lungonly_curated",
-                                            "repeats"), labels=c("Genes","SNPs","INDELs",
-                                                                 "INDELs_curated",
+                                            "repeats"), labels=c("Genes",
+                                                                 "SNPs",
+                                                                 "INDELs",
                                                                  "Repeats"))
 
 ggplot(df, aes(Window, Density)) + geom_line(aes(color=Chromosome),
