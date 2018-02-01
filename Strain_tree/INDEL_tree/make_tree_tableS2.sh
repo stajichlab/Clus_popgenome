c400e73c56f9efdae05d663932b2c39c23c4365b
Jason Stajich <jasonstajich.phd@gmail.com>
2018-02-01 12:09:40 -0800

    run morpho and binary character search

--- Strain_tree/INDEL_tree/make_tree_tableS2.sh
+++ Strain_tree/INDEL_tree/make_tree_tableS2.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-#SBATCH --ntasks 2 --nodes 1 --out iqtree.%A.outlog --mem 2G
-
+#SBATCH --ntasks 2 --nodes 1 --out iqtree.%A.outlog --mem 2G --time 8:00:00
 module load IQ-TREE
-iqtree-omp -nt 2 -s Table_S2_indel_matrix.fasaln -m JC2+ASC  -b 100 
+
+#iqtree-omp -nt 2 -s Table_S2_indel_matrix.fasaln -m JC2+ASC  -b 100 
+iqtree-omp -nt 2 -s Table_S2_indel_matrix.fasaln -st MORPH -b 100 
+iqtree-omp -nt 2 -s Table_S2_indel_matrix_binaryonly.fasaln -st JSC2+ASC -b 100 
